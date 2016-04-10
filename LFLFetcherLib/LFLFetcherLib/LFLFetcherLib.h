32ae966c3578caee68eb39dca074f06b966f44d3
zhangxiaofeng <zhangxiaofeng01@baidu.com>
2016-04-10 12:46:32 +0800

    message: 新增fetcherLib 用来处理网络请求，数据库操作等

---
+++ LFLFetcherLib/LFLFetcherLib/LFLFetcherLib.h
@@ -0,0 +1,13 @@
+//
+//  LFLFetcherLib.h
+//  LFLFetcherLib
+//
+//  Created by 啸峰 on 16/4/8.
+//  Copyright © 2016年 张啸峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LFLFetcherLib : NSObject
+
+@end
---
+++ LFLFetcherLib/LFLFetcherLib/LFLFetcherLib.m
@@ -0,0 +1,13 @@
+//
+//  LFLFetcherLib.m
+//  LFLFetcherLib
+//
+//  Created by 啸峰 on 16/4/8.
+//  Copyright © 2016年 张啸峰. All rights reserved.
+//
+
+#import "LFLFetcherLib.h"
+
+@implementation LFLFetcherLib
+
+@end
